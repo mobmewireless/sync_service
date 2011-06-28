8ca3fc012123afdd2193b44f88c0c5427dc86c75
Hari Gopal <hari@mobme.in>
2011-06-28 17:57:02 +0530

    Adding documentation for untested line in runner.

--- lib/mobme/infrastructure/rpc/runner.rb
+++ lib/mobme/infrastructure/rpc/runner.rb
@@ -2,6 +2,7 @@ module MobME::Infrastructure::RPC
   class Runner
     def self.start(application, host, port, route)
       Thin::Server.start(host, port) do
+        # Since no logger is specified, this will log apache-style strings to STDERR for each request.
         use Rack::CommonLogger
 
         map route do
